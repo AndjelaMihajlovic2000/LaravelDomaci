fa745ac679ec2c5dd0dcf615974c1aa82f352905
AndjelaMihajlovic2000 <mihajlovica41@gmail.com>
2022-07-12 23:50:21 +0200

    Kreirana autentifikacija

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+
+    public function login(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+
+        $user = User::where("email", $email)->first();
+
+        if (!$user) {
+            return response()->json([
+                "error" => "Bad credentials"
+            ], 400);
+        }
+        if (!Hash::check($password, $user->password)) {
+            return response()->json([
+                "error" => "Bad credentials"
+            ], 400);
+        }
+        return response()->json([
+            "user" => $user,
+            "token" => $user->createToken()->plainTextToken
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+        $name = $request->name;
+        $user = User::where("email", $email)->first();
+        if ($user) {
+            return response()->json([
+                "error" => "User already exists"
+            ], 400);
+        }
+
+        $createdUser = User::create([
+            "email" => $email,
+            "name" => $name,
+            "password" => Hash::make($password)
+        ]);
+        return response()->json([
+            "user" => $createdUser,
+            "token" => $createdUser->createToken()->plainTextToken
+        ]);
+    }
+
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth-sanctum')->post('/logout', [AuthController::class, 'logout']);
