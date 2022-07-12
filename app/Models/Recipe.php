0d883b179e6b1c0504b2beb3877d4284fbb44178
AndjelaMihajlovic2000 <mihajlovica41@gmail.com>
2022-07-12 23:22:01 +0200

    Kreirani modeli i migracije

---
+++ app/Models/Recipe.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'author', 'publish_date', 'description'];
+
+
+    public function steps()
+    {
+        return $this->hasMany(Step::class);
+    }
+
+    public function ingredients()
+    {
+        return $this->hasMany(Ingredient::class);
+    }
+}
---
+++ database/migrations/2022_07_12_210216_create_recipes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('author');
+            $table->date('publish_date');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
